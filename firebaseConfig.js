b87878fad4748bf9d0fb0117e7106036ee43f37f
vanshxo <khatrivansh43@gmail.com>
2024-12-21 16:13:09 +0530

    testing

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+export const firebaseConfig = {
+    apiKey: "AIzaSyCeUBxN-llJciMNdom9Irb9vb0iO8j-nh0",
+    authDomain: "re-verse-877bf.firebaseapp.com",
+    projectId: "re-verse-877bf",
+    storageBucket: "re-verse-877bf.firebasestorage.app",
+    messagingSenderId: "910625117429",
+    appId: "1:910625117429:web:eb68caf05a267bfbfee1bc",
+    measurementId: "G-PNJB6VZPD2"
+  };
+
+
+export const app = initializeApp(firebaseConfig);
